070f832fe134e8627a58d08574e16a66f64678f7
alexis <alexisrichmond7gmail.com>
2021-08-28 14:46:48 +0200

    second link

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,11 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController 
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response("hello world!!!");
-    }  
+    } 
+    /** 
+     * @Route("/questions/{slug}") 
+     */ 
+    public function show($slug)  
+    {
+        return new Response(sprintf(
+            "la page qui affiche les reponses est %s  ", $slug
+        ));
+    }
 }
